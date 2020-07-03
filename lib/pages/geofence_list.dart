e4f26cd2c58b446a8e236aa820a28f71743eb96e
Shiburagi <tr32010@gmail.com>
2020-07-04 05:22:13 +0800

    handle empty list

--- lib/pages/geofence_list.dart
+++ lib/pages/geofence_list.dart
@@ -14,7 +14,9 @@ class _GeofenceListPageState extends BlocState<GeofenceListPage, GeofenceBloc> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Geofence List"),
+      ),
       floatingActionButton: FloatingActionButton.extended(
           heroTag: "action_geofence",
           onPressed: () => Navigator.of(context).pushNamed("/geofence/add"),
@@ -23,46 +25,67 @@ class _GeofenceListPageState extends BlocState<GeofenceListPage, GeofenceBloc> {
       body: StreamBuilder<List<Geofence>>(
           stream: bloc.startStream(),
           builder: (context, snapshot) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                Geofence geofence = bloc.data[index];
-                String radiusText;
-                if (geofence.radius >= 1000)
-                  radiusText = "${geofence.radius / 1000.0}km";
-                else
-                  radiusText = "${geofence.radius}m";
-                return Card(
-                  child: ListTile(
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 8).copyWith(left: 16),
-                    title: Text(geofence.wifiName ?? geofence.bssid),
-                    subtitle: Text(
-                        "${geofence.latitude}, ${geofence.longitude} ($radiusText)"),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Container(
-                          height: 32,
-                          width: 1,
-                          color: Theme.of(context).dividerColor,
+            return snapshot.data?.isEmpty ?? true
+                ? Center(
+                    child: Card(
+                      margin: EdgeInsets.all(8).copyWith(bottom: 32),
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                        child: Text(
+                          "No geofence found.",
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle1
+                              .copyWith(color: Theme.of(context).hintColor),
                         ),
-                        IconButton(
-                            icon: Icon(Icons.edit),
-                            onPressed: () => bloc.edit(context, geofence)),
-                        IconButton(
-                            icon: Icon(
-                              Icons.delete,
-                              color: Theme.of(context).errorColor,
-                            ),
-                            onPressed: () => bloc.delete(geofence)),
-                      ],
+                      ),
                     ),
-                  ),
-                );
-              },
-              itemCount: bloc.data?.length ?? 0,
-            );
+                  )
+                : buildGeofenceList();
           }),
+    );
+  }
+
+  ListView buildGeofenceList() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        Geofence geofence = bloc.data[index];
+        String radiusText;
+        if (geofence.radius >= 1000)
+          radiusText = "${geofence.radius / 1000.0}km";
+        else
+          radiusText = "${geofence.radius}m";
+        return Card(
+          child: ListTile(
+            contentPadding:
+                EdgeInsets.symmetric(vertical: 8).copyWith(left: 16),
+            title: Text(geofence.wifiName ?? geofence.bssid),
+            subtitle: Text(
+                "${geofence.latitude}, ${geofence.longitude} ($radiusText)"),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 32,
+                  width: 1,
+                  color: Theme.of(context).dividerColor,
+                ),
+                IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () => bloc.edit(context, geofence)),
+                IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Theme.of(context).errorColor,
+                    ),
+                    onPressed: () => bloc.delete(geofence)),
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: bloc.data?.length ?? 0,
     );
   }
 }
